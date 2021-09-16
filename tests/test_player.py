fcd4a71150ea77dcfc449cf30f469ee427c0785c
nualagr <nualagreenwood@gmail.com>
2021-09-16 15:26:47 +0100

    feat: Add Player Class.
    Dependency Injection: Each Player Object is instantiated with a name
    and a hand attribute. The hand attribute stores a Hand object.
    The Player's best_hand instance method returns the result of a call to
    the hand attribute/object's best_rank() method.
    
    fix: Add _no_cards() instance method to Hand Class to return 'No Cards'
    and not 'High Card' when .best_rank() is called on a hand containing an
    empty list of cards.
    
    fix: Return False within the _royal_flush instance method
    if is_straight_flush evaluates to False as this means that
    there are fewer than five cards in the hand. This prevents
    an IndexError when the is_royal variable tries to index
    into the empty list.

---
+++ tests/test_player.py
@@ -0,0 +1,25 @@
+import unittest
+from unittest.mock import MagicMock
+from poker.card import Card
+from poker.hand import Hand
+from poker.player import Player
+
+class PlayerTest(unittest.TestCase):
+    def test_stores_name_and_hand(self):
+        hand = Hand(cards = [])
+        player = Player(name = "Barry", hand = hand)
+        self.assertEqual(player.name, "Barry")
+        self.assertEqual(player.hand, hand)
+    
+    def test_figures_out_own_best_hand(self):
+        mock_hand = MagicMock()
+        mock_hand.best_rank.return_value = "Straight Flush"
+
+        player = Player(name = "Barry", hand = mock_hand)
+        
+        self.assertEqual(
+            player.best_hand(),
+            "Straight Flush"
+        )
+        
+        mock_hand.best_rank.assert_called()

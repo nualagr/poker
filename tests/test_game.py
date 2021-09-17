9a6ced3d078d60ee87fc19f13d8d6d5021fb91e2
nualagr <nualagreenwood@gmail.com>
2021-09-17 16:21:16 +0100

    feat: Send Initial Two Cards to the Players.
    Add _deal_initial_two_cards_to_each_player() instance method.
    Extract the shuffle-deck logic out of play into a spearate instance method.
    When play() is called on Game _shuffle_deck() and
    _deal_initial_two_cards_to_each_player() are called in turn.
    
    Expand unittest test_deal_two_initial_cards_from_deck_to_each_player
    Create lists of the first two cards and the second two cards dealt.
    Use assert_called_with to verify that the add_cards method was called
    on each mock_player with the two different lists of cards.

--- tests/test_game.py
+++ tests/test_game.py
@@ -1,6 +1,7 @@
 import unittest
 from unittest.mock import MagicMock, call
 
+from poker.card import Card
 from poker.game import Game
 
 class GameTest(unittest.TestCase):
@@ -43,12 +44,21 @@ class GameTest(unittest.TestCase):
         mock_deck.shuffle.assert_called_once()
     
     def test_deals_two_initial_cards_from_deck_to_each_player(self):
-        mock_deck = MagicMock()
-
-        players = [
-            MagicMock(),
-            MagicMock()
+        first_two_cards = [
+            Card(rank = "3", suit = "Hearts"),
+            Card(rank = "9", suit = "Diamonds")
         ]
+        second_two_cards = [
+            Card(rank = "Jack", suit = "Clubs"),
+            Card(rank = "10", suit = "Clubs")
+        ]
+
+        mock_deck = MagicMock()
+        mock_deck.remove_cards.side_effect = [first_two_cards, second_two_cards]
+
+        mock_player1 = MagicMock()
+        mock_player2 = MagicMock()
+        players = [mock_player1, mock_player2]
 
         game = Game(
             deck = mock_deck,
@@ -61,4 +71,7 @@ class GameTest(unittest.TestCase):
         # Test that remove_cards was called with the argument 2 each time
         mock_deck.remove_cards.assert_has_calls(
             [call(2), call(2)]
-        )       
+        )    
+        
+        mock_player1.add_cards.assert_called_with(first_two_cards),
+        mock_player2.add_cards.assert_called_with(second_two_cards)

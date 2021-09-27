121ca8d99e6488bf730722ceae6024a5f5918375
nualagr <nualagreenwood@gmail.com>
2021-09-27 15:51:16 +0100

    refactor: Refactoring the Hand Class.
    Remove the '_rank_validations_from_best_to_worst' property.
    Replace with a VALIDATORS Class Attribute in the same order.
    Each Validator Object no longer has to be instantiated unnecessarily.
    Within the 'best_rank' instance method use a 'for' loop to
    iterate over the VALIDATORS attribute. For each validator,
    instantiate it with the self.cards list.
    Use an 'if' statement to check whether the current validator 'is_valid'
    If so, return the validator name.
    Subsequent Validator objects will now only be instantiated
    if previous ones evalutate to false.

--- tests/test_hand.py
+++ tests/test_hand.py
@@ -1,6 +1,7 @@
 import unittest
 from poker.card import Card
 from poker.hand import Hand
+from poker.validators import PairValidator
 
 
 class HandTest(unittest.TestCase):
@@ -23,3 +24,23 @@ class HandTest(unittest.TestCase):
         hand.add_cards(cards)
 
         self.assertEqual(hand.cards, [six_of_clubs, ace_of_spades])
+
+    def test_interacts_with_validator_to_get_winning_hand(self):
+        class HandWithOneValidator(Hand):
+            # Override the SuperClass VALIDATORS attribute
+            # to be a tuple of one. This is enough to test the
+            # 'best_rank' method.
+            VALIDATORS = (PairValidator,)
+
+        ace_of_hearts = Card(rank="Ace", suit="Hearts")
+        ace_of_spades = Card(rank="Ace", suit="Spades")
+
+        cards = [
+            ace_of_hearts,
+            ace_of_spades,
+        ]
+
+        hand = HandWithOneValidator()
+        hand.add_cards(cards=cards)
+
+        self.assertEqual(hand.best_rank(), "Pair")
